ffdf96cf2d74cf357211be145d5e0bd1880846f0
OluwaFavour <fsticks8187@gmail.com>
2023-03-27 04:36:57 +0000

    Add prototype

--- main.h
+++ main.h
@@ -42,6 +42,7 @@ int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
+int _printf(const char *format, ...);
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
